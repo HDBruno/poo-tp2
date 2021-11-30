3d64360a17346852531cc1b1cd28d3cb80d0d682
Herni <hernan.bruno@outlook.com>
2021-11-30 15:27:43 -0300

    First commit

---
+++ src/main/java/ar/edu/unnoba/poo/PooTp2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooTp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooTp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/poo/PooTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooTp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
